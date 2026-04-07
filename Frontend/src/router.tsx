9e7c60db4bb90744a478054e8919ed99536e398a
Alejandro Correa Arias <alejoariascorrea@gmail.comm>
2026-04-07 00:27:14 -0500

    Avance: Cree el jwt, navegacion, nuevas pantallas, validacion y nuevas funciones en el back

--- Frontend/src/router.tsx
+++ Frontend/src/router.tsx
@@ -1,15 +1,23 @@
-import{BrowserRouter, Routes, Route} from 'react-router-dom'
-import LoginView from './views/LoginView'   
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import LoginView from './views/LoginView'
 import RegisterView from './views/RegisterView'
 import AuthLayout from './layouts/AuthLayout'
-export default function Router(){
-    return(
+import AppLayout from './layouts/App.Layout'
+import LinkTreeView from './views/LinkTreeView'
+import ProfileView from './views/ProfileView'
+export default function Router() {
+    return (
         <BrowserRouter>
             <Routes>
-               <Route element={<AuthLayout/>}>
-                <Route path='/auth/login' element={<LoginView/>}/>
-                   <Route path='/auth/register' element={<RegisterView/>}/>
-             </Route>  
+                <Route element={<AuthLayout />}>
+                    <Route path='/auth/login' element={<LoginView />} />
+                    <Route path='/auth/register' element={<RegisterView />} />
+                </Route>
+
+                <Route path='/admin' element={<AppLayout />}>
+                    <Route index={true} element={<LinkTreeView />} />
+                    <Route path='profile' element={<ProfileView />} />
+                </Route>
             </Routes>
         </BrowserRouter>
     )
